fdb986cf97b56e90eb9ab4403cac8163a42768a2
NgoXCuong <ngocuongcm2005@gmail.com>
2025-07-23 15:18:57 +0700

    send report

--- frontend/js/report.js
+++ frontend/js/report.js
@@ -1,4 +1,5 @@
 // === VALIABLES ===
+const apiKey = "aeb2298dd49b3aa479ae2f6e0d6fcbf2";
 const enpoin = "https://68803af6f1dcae717b615ab0.mockapi.io/scammers";
 const uploadImageInput = document.getElementById("uploadImage");
 let arrImg = [];
@@ -7,10 +8,10 @@ const formUploadWrap = document.querySelector(".form__upload-wrap");
 // === HANDLE UPLOAD IMAGE ===
 uploadImageInput.addEventListener("change", handleUploadImage);
 
-function handleUploadImage(e) {
+async function handleUploadImage(e) {
   const imgList = [...e.target.files];
-  imgList.forEach((item) => {
-    const urlImg = URL.createObjectURL(item);
+  imgList.forEach(async (item) => {
+    const urlImg = await uploadImgBB(item);
     const imgHTML = `<div class="form__image-preview">
           <div class="form__preview-remove" data-url="${urlImg}">
             <i class="fa-solid fa-xmark"></i>
@@ -60,6 +61,9 @@ Validator({
         images: arrImg,
         ...rest,
       });
+      const formImgPreviews = document.querySelectorAll(".form__image-preview");
+      formImgPreviews.forEach((item) => item.remove());
+      await FuiToast.success("Gửi tố cáo thành công");
     } catch (error) {
       console.log(error);
     }
@@ -68,3 +72,25 @@ Validator({
   },
   resetOnSubmit: true,
 });
+
+// === HANDLE UPLOAD IMGBB
+async function uploadImgBB(file) {
+  const formData = new FormData();
+  // Thêm file hình ảnh vào FormData để gửi lên server
+  formData.append("image", file);
+
+  try {
+    // Gửi yêu cầu POST đến API ImgBB với đường dẫn endpoint và apiKey bằng axios
+    const response = await axios.post(
+      `https://api.imgbb.com/1/upload?key=${apiKey}`,
+      formData
+    );
+
+    // Trả về URL của hình ảnh sau khi tải lên thành công
+    return response.data.data.url;
+  } catch (error) {
+    // Bắt lỗi nếu có vấn đề xảy ra trong quá trình tải lên
+    console.log(error);
+    return null; // Trả về null nếu gặp lỗi
+  }
+}
