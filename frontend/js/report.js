0f08499842ece3236cf29c25e5cf987d0920fb52
NgoXCuong <ngocuongcm2005@gmail.com>
2025-07-23 08:45:03 +0700

    test repost mockapi

--- frontend/js/report.js
+++ frontend/js/report.js
@@ -1,4 +1,5 @@
 // === VALIABLES ===
+const enpoin = "https://68803af6f1dcae717b615ab0.mockapi.io/scammers";
 const uploadImageInput = document.getElementById("uploadImage");
 let arrImg = [];
 const formUploadWrap = document.querySelector(".form__upload-wrap");
@@ -20,6 +21,7 @@ function handleUploadImage(e) {
     arrImg.push(urlImg);
   });
 }
+
 formUploadWrap.addEventListener("click", (e) => {
   if (e.target.matches(".form__preview-remove")) {
     const formImgPreview = e.target.parentNode;
@@ -35,24 +37,33 @@ Validator({
   formGroupSelector: ".form__group",
   errorSelector: ".form-message",
   rules: [
-    // Validator.isRequired("#nameScammer", "Vui lòng nhập tên chủ tài khoản"),
-    // Validator.minLength("#nameScammer", 5, "Nhập tối thiểu 5 ký tự"),
-    // Validator.isRequired("#phoneScammer", "Vui lòng nhập số điện thoại"),
-    // Validator.isPhoneNumber("#phoneScammer", "Số điện thoại không hợp lệ"),
-    // Validator.isRequired("#numberScammer", "Vui lòng nhập số tài khoản"),
-    // Validator.isNumber("#numberScammer"),
-    // Validator.minLength("#numberScammer", 8, "Nhập tối thiểu 8 ký tự"),
-    // Validator.isRequired("#bankScammer", "Vui lòng nhập tên ngân hàng"),
-    // Validator.minLength("#bankScammer", 2, "Nhập tối thiểu 2 ký tự"),
-    // Validator.isRequired("#contentReport", "Vui lòng nhập nội dung tố cáo"),
-    // Validator.minLength("#contentReport", 20, "Nhập tối thiểu 20 ký tự"),
-    // Validator.isImageUploaded("#uploadImage", "Vui lòng upload hình ảnh"),
-    // Validator.isRequired("#nameUser", "Vui lòng nhập họ tên của bạn"),
-    // Validator.minLength("#nameUser", 5, "Nhập tối thiểu 5 ký tự"),
-    // Validator.isRequired("#phoneUser", "Vui lòng nhập số điện thoại"),
-    // Validator.isPhoneNumber("#phoneUser", "Số điện thoại không hợp lệ"),
+    Validator.isRequired("#nameScammer", "Vui lòng nhập tên chủ tài khoản"),
+    Validator.minLength("#nameScammer", 5, "Nhập tối thiểu 5 ký tự"),
+    Validator.isRequired("#phoneScammer", "Vui lòng nhập số điện thoại"),
+    Validator.isPhoneNumber("#phoneScammer", "Số điện thoại không hợp lệ"),
+    Validator.isRequired("#numberScammer", "Vui lòng nhập số tài khoản"),
+    Validator.isNumber("#numberScammer"),
+    Validator.minLength("#numberScammer", 8, "Nhập tối thiểu 8 ký tự"),
+    Validator.isRequired("#bankScammer", "Vui lòng nhập tên ngân hàng"),
+    Validator.minLength("#bankScammer", 2, "Nhập tối thiểu 2 ký tự"),
+    Validator.isRequired("#contentReport", "Vui lòng nhập nội dung tố cáo"),
+    Validator.minLength("#contentReport", 20, "Nhập tối thiểu 20 ký tự"),
+    Validator.isImageUploaded("#uploadImage", "Vui lòng upload hình ảnh"),
+    Validator.isRequired("#nameUser", "Vui lòng nhập họ tên của bạn"),
+    Validator.minLength("#nameUser", 5, "Nhập tối thiểu 5 ký tự"),
+    Validator.isRequired("#phoneUser", "Vui lòng nhập số điện thoại"),
+    Validator.isPhoneNumber("#phoneUser", "Số điện thoại không hợp lệ"),
   ],
-  onSubmit: function ({ images, ...rest }) {
+  onSubmit: async function ({ images, ...rest }) {
+    try {
+      await axios.post(enpoin, {
+        images: arrImg,
+        ...rest,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
     console.log({ images: arrImg, ...rest }); // Dữ liệu form sau khi xác thực thành công
   },
   resetOnSubmit: true,
