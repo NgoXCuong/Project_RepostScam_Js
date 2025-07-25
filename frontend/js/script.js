dad029f4a62aa861bdb910aae4484ec0e84c1337
NgoXCuong <ngocuongcm2005@gmail.com>
2025-07-25 18:40:34 +0700

    show data model

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,92 +1,9 @@
-const modelHTML = `<section class="model">
-        <div class="model__overlay"></div>
-        <div class="model__content">
-          <div class="model__header">
-            <div class="model__header-title">Chi tiết tố cáo</div>
-            <div class="model__header-close">
-              <i class="fa-solid fa-xmark"></i>
-            </div>
-          </div>
-          <div class="model__body">
-            <div class="model__group">
-              <div class="model__profile">
-                <div class="model__profile-avatar">
-                  <img
-                    src="./assets/img/avatar.png"
-                    alt=""
-                    style="width: 40px; height: 40px"
-                  />
-                </div>
-                <div class="model__info">
-                  <h4 class="model__info-name">Hà Thị Yến Nhi</h4>
-                  <p class="model__info-desc">#50 - Tố cáo ngày 19/07/2025</p>
-                </div>
-              </div>
-              <div class="model__detail">
-                <span class="model__detail-title">Số điện thoại</span>
-                <span class="model__detail-text">0388 098 123</span>
-              </div>
-              <div class="model__detail">
-                <span class="model__detail-title">Số tài khoản</span>
-                <span class="model__detail-text">0388 098 123</span>
-              </div>
-              <div class="model__detail">
-                <span class="model__detail-title">Ngân hàng</span>
-                <span class="model__detail-text">MB Bank</span>
-              </div>
-            </div>
-            <div class="model__group">
-              <div class="model__profile">
-                <div class="model__profile-avatar">
-                  <img
-                    src="./assets/img/avatar.png"
-                    alt=""
-                    style="width: 40xp; height: 40px"
-                  />
-                </div>
-                <div class="model__info">
-                  <h4 class="model__info-name">Ngô Xuân Cường</h4>
-                  <p class="model__info-desc">Người tố cáo</p>
-                </div>
-              </div>
-              <div class="model__detail">
-                <span class="model__detail-title">Trạng thái</span>
-                <span class="model__detail-text">Nạn nhân</span>
-              </div>
-              <div class="model__detail">
-                <span class="model__detail-title">Liên hệ</span>
-                <span class="model__detail-text">0123 456 789</span>
-              </div>
-              <div class="model__textarea">
-                <span class="model__detail-title">Nội dung tố cáo</span>
-                <p class="model__textarea-content">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Officia soluta ipsam nisi qui, dolor non quas. Iure asperiores
-                  a at impedit minus qui corporis repudiandae. Itaque aliquid
-                  debitis excepturi natus.
-                </p>
-              </div>
-              <div class="model__image">
-                <span class="model__detail-title">Hình ảnh liên quan</span>
-                <div class="model__preview-image">
-                  <img src="./assets/img/shield.png" alt="" />
-                  <img src="./assets/img/shield.png" alt="" />
-                  <img src="./assets/img/shield.png" alt="" />
-                  <img src="./assets/img/shield.png" alt="" />
-                  <img src="./assets/img/shield.png" alt="" />
-                  <img src="./assets/img/shield.png" alt="" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>`;
-const scammerItems = document.querySelectorAll(".scammer__item");
 const warningHeaders = document.querySelectorAll(".warning__header");
 const endpoint = "https://68803af6f1dcae717b615ab0.mockapi.io/scammers";
 const scammerList = document.querySelector(".scammer__list");
 const today = document.querySelector(".today");
 const alertScamDesc = document.querySelector(".alert-scam__desc");
+let scammerData = [];
 
 // === HANDLE FORMAT DATE ===
 function formatDate(dateString) {
@@ -134,9 +51,92 @@ function handleShowDropdown(e) {
 }
 
 // === MODEL ===
-scammerItems.forEach((item) => item.addEventListener("click", handleShowModel));
-
-function handleShowModel() {
+// scammerItems.forEach((item) => item.addEventListener("click", handleShowModel));
+function handleShowModel(id) {
+  const scammer = scammerData.find((item) => item.id === id);
+  const imgListHTML = scammer.images
+    .map((item) => {
+      return `<img src="${item}" alt="" />`;
+    })
+    .join("");
+  console.log(scammer);
+  const modelHTML = `<section class="model">
+        <div class="model__overlay"></div>
+        <div class="model__content">
+          <div class="model__header">
+            <div class="model__header-title">Chi tiết tố cáo</div>
+            <div class="model__header-close">
+              <i class="fa-solid fa-xmark"></i>
+            </div>
+          </div>
+          <div class="model__body">
+            <div class="model__group">
+              <div class="model__profile">
+                <div class="model__profile-avatar">
+                  <img
+                    src="./assets/img/avatar.png"
+                    alt=""
+                    style="width: 40px; height: 40px"
+                  />
+                </div>
+                <div class="model__info">
+                  <h4 class="model__info-name">${scammer.nameScammer}</h4>
+                  <p class="model__info-desc">#${
+                    scammer.id
+                  } - Tố cáo ngày ${formatDate(scammer.date)}</p>
+                </div>
+              </div>
+              <div class="model__detail">
+                <span class="model__detail-title">Số điện thoại</span>
+                <span class="model__detail-text">${scammer.phoneScammer}</span>
+              </div>
+              <div class="model__detail">
+                <span class="model__detail-title">Số tài khoản</span>
+                <span class="model__detail-text">${scammer.numberScammer}</span>
+              </div>
+              <div class="model__detail">
+                <span class="model__detail-title">Ngân hàng</span>
+                <span class="model__detail-text">${scammer.bankScammer}</span>
+              </div>
+            </div>
+            <div class="model__group">
+              <div class="model__profile">
+                <div class="model__profile-avatar">
+                  <img
+                    src="./assets/img/avatar.png"
+                    alt=""
+                    style="width: 40xp; height: 40px"
+                  />
+                </div>
+                <div class="model__info">
+                  <h4 class="model__info-name">${scammer.nameUser}</h4>
+                  <p class="model__info-desc">Người tố cáo</p>
+                </div>
+              </div>
+              <div class="model__detail">
+                <span class="model__detail-title">Trạng thái</span>
+                <span class="model__detail-text">${scammer.option}</span>
+              </div>
+              <div class="model__detail">
+                <span class="model__detail-title">Liên hệ</span>
+                <span class="model__detail-text">${scammer.numberScammer}</span>
+              </div>
+              <div class="model__textarea">
+                <span class="model__detail-title">Nội dung tố cáo</span>
+                <p class="model__textarea-content">
+                  ${scammer.contentReport}
+                </p>
+              </div>
+              <div class="model__image">
+                <span class="model__detail-title">Hình ảnh liên quan</span>
+                <div class="model__preview-image">
+                  ${imgListHTML}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>`;
   document.body.insertAdjacentHTML("afterbegin", modelHTML);
   document.body.style.overflow = "hidden";
 }
@@ -149,6 +149,8 @@ document.body.addEventListener("click", (e) => {
   ) {
     model.remove();
     document.body.style.overflow = "auto";
+  } else if (e.target.matches(".scammer__item")) {
+    handleShowModel(e.target.dataset.id);
   }
 });
 
@@ -165,7 +167,7 @@ function rederScammerToday(data) {
 
   if (todayData && todayData.length > 0) {
     todayData.forEach((item) => {
-      const scammerItemHTML = `<li class="scammer__item">
+      const scammerItemHTML = `<li class="scammer__item" data-id = "${item.id}">
             <img src="./assets/img/avatar.png" alt="" class="scammer__avatar" />
             <div class="scammer__info">
               <h3 class="scammer__name">${item.nameScammer}</h3>
@@ -183,8 +185,8 @@ function rederScammerToday(data) {
 async function getScammer() {
   try {
     const response = await axios.get(endpoint);
-    const data = await response.data;
-    rederScammerToday(data);
+    scammerData = await response.data;
+    rederScammerToday(scammerData);
   } catch (error) {
     console.error(error);
   }
